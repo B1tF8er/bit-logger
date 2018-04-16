20ae56446c02f179379b3fdeb5c99cd4e029407c
Jorge Yañez <ofeth@hotmail.com>
2018-04-16 17:34:01 -0500

    Added validation to provide a custom format for the messages

--- Bit.Logger/Config/Configuration.cs
+++ Bit.Logger/Config/Configuration.cs
@@ -2,12 +2,14 @@ namespace Bit.Logger.Config
 {
     public class Configuration
     {
-        public bool ShowDate { get; set; } = false;
+        public bool ShowDate { get; set; } = true;
 
-        public bool ShowTime { get; set; } = false;
+        public bool ShowTime { get; set; } = true;
 
         public bool ShowLevel { get; set; } = true;
 
         public Level Level { get; set; } = Level.Information;
+
+        public bool UseCustomFormat { get; set; } = false;
     }
 }
--- Bit.Logger/Config/FileConfiguration.cs
+++ Bit.Logger/Config/FileConfiguration.cs
@@ -7,7 +7,38 @@ namespace Bit.Logger.Config
     {
         public IFormatProvider FormatProvider { get; set; } = new LoggerFormatProvider();
 
-        public string Format { get; set; } = "{0:level} {1:datetime} [{2}::{3}] {4} {5:exception}";
+        private string _format;
+
+        public string Format
+        {
+            get
+            {
+                if (!UseCustomFormat)
+                {
+                    _format = string.Empty;
+
+                    if (ShowLevel)
+                        _format += "{0:level}";
+                    if (ShowDate && ShowTime)
+                        _format += "{1:datetime}";
+                    if (ShowDate)
+                        _format += "{1:date}";
+                    if (ShowTime)
+                        _format += "{1:time}";
+
+                    _format += "[{2}::{3}] {4} {5:exception}";
+                }
+
+                return _format;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(_format) || string.IsNullOrWhiteSpace(_format))
+                    throw new ArgumentNullException(nameof(_format));
+
+                _format = value;
+            }
+        }
 
         public string FilePath { get; set; } = string.Empty;
     }
