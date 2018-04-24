c47196ffd429e065a650cd6508e167d2f29a0662
Jorge Yañez <ofeth@hotmail.com>
2018-04-24 15:02:21 -0500

    Added more samples

---
+++ Bit.Logger.Samples/Constants.cs
@@ -0,0 +1,23 @@
+namespace Bit.Logger.Samples
+{
+    using System;
+
+    internal class Constants
+    {
+        internal const string TraceMessage = "TEST TRACE MESSAGE";
+        internal const string DebugMessage = "TEST DEBUG MESSAGE";
+        internal const string VerboseMessage = "TEST VERBOSE MESSAGE";
+        internal const string InformationMessage = "TEST INFORMATION MESSAGE";
+        internal const string WarningMessage = "TEST WARNING MESSAGE";
+        internal const string ErrorMessage = "TEST ERROR MESSAGE";
+        internal const string CriticalMessage = "TEST CRITICAL MESSAGE";
+
+        internal static readonly Exception TraceException = new Exception("TEST TRACE EXCEPTION");
+        internal static readonly Exception DebugException = new Exception("TEST DEBUG EXCEPTION");
+        internal static readonly Exception VerboseException = new Exception("TEST VERBOSE EXCEPTION");
+        internal static readonly Exception InformationException = new Exception("TEST INFORMATION EXCEPTION");
+        internal static readonly Exception WarningException = new Exception("TEST WARNING EXCEPTION");
+        internal static readonly Exception ErrorException = new Exception("TEST ERROR EXCEPTION");
+        internal static readonly Exception CriticalException = new Exception("TEST CRITICAL EXCEPTION");
+    }
+}
--- Bit.Logger.Samples/Program.cs
+++ Bit.Logger.Samples/Program.cs
@@ -12,17 +12,20 @@
         {
             var consoleConfiguration = new Configuration()
             {
-                Level = Level.Critical
+                Level = Level.Trace,
+                ShowLevel = false
             };
 
             var databaseConfiguration = new Configuration()
             {
-                Level = Level.Critical
+                Level = Level.Critical,
+                ShowTime = false,
             };
 
             var fileConfiguration = new Configuration()
             {
-                Level = Level.Critical
+                Level = Level.Information,
+                ShowDate = false
             };
             
             logger
@@ -30,18 +33,42 @@
                 .AddDatabaseSource(databaseConfiguration)
                 .AddFileSource(fileConfiguration);
 
-            SampleLogging();
+            SampleMessageLogs();
+            SampleExceptionLogs();
+            SampleMessageAndExceptionLogs();
         }
 
-        static void SampleLogging()
+        static void SampleMessageLogs()
         {
-            logger.Trace("test");
-            logger.Debug("test");
-            logger.Verbose("test");
-            logger.Information("test");
-            logger.Warning("test");
-            logger.Error("test");
-            logger.Critical("test");
+            logger.Trace(Constants.TraceMessage);
+            logger.Debug(Constants.DebugMessage);
+            logger.Verbose(Constants.VerboseMessage);
+            logger.Information(Constants.InformationMessage);
+            logger.Warning(Constants.WarningMessage);
+            logger.Error(Constants.ErrorMessage);
+            logger.Critical(Constants.CriticalMessage);
+        }
+        
+        static void SampleExceptionLogs()
+        {
+            logger.Trace(Constants.TraceException);
+            logger.Debug(Constants.DebugException);
+            logger.Verbose(Constants.VerboseException);
+            logger.Information(Constants.InformationException);
+            logger.Warning(Constants.WarningException);
+            logger.Error(Constants.ErrorException);
+            logger.Critical(Constants.CriticalException);
+        }
+
+        static void SampleMessageAndExceptionLogs()
+        {
+            logger.Trace(Constants.TraceMessage, Constants.TraceException);
+            logger.Debug(Constants.DebugMessage, Constants.DebugException);
+            logger.Verbose(Constants.VerboseMessage, Constants.VerboseException);
+            logger.Information(Constants.InformationMessage, Constants.InformationException);
+            logger.Warning(Constants.WarningMessage, Constants.WarningException);
+            logger.Error(Constants.ErrorMessage, Constants.ErrorException);
+            logger.Critical(Constants.CriticalMessage, Constants.CriticalException);
         }
     }
 }
