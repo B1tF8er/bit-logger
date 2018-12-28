8f7b86d5dd88321eb5359428600ee5c2590742cd
B1tF8er <ofeth@hotmail.com>
2018-12-28 06:40:38 -0700

    Remove unused constants

--- Bit.Logger.Tests/Constants.cs
+++ Bit.Logger.Tests/Constants.cs
@@ -12,15 +12,8 @@ namespace Bit.Logger.Tests
         internal const string TimeToken = "time";
         internal const string CallerToken = "caller";
         internal const string ExceptionToken = "exception";
-        internal const string MethodToken = "method";
-        internal const string MessageToken = "message";
         internal const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
         internal const string DateFormat = "yyyy-MM-dd";
         internal const string TimeFormat = "HH:mm:ss";
-        internal const string LogName = "BitLogger";
-        internal const string LogNameFormat = "yyyy_MM_dd_HH";
-        internal const string ConnectionString = "Data Source=logging.db";
-        internal const string AsKey = "yyyy-MM-dd HH:mm:ss.fff";
-        internal const int LogsThreshold = 500;
     }
 }
