33220c671f1cdde96e96dec8ad0c61bc4d3eb110
B1tF8er <ofeth@hotmail.com>
2018-12-28 01:34:58 -0700

    Remove VersionNumber.bat

--- VersioNumber.bat
+++
@@ -1,6 +0,0 @@
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (
-Set suffix=%APPVEYOR_BUILD_NUMBER%
-) ELSE (
-Set suffix=""
-ECHO ^<Project^>^<PropertyGroup^>^<VersionPrefix^>%APPVEYOR_REPO_TAG_NAME%^</VersionPrefix^>^</PropertyGroup^>^</Project^> > version.props
-)
