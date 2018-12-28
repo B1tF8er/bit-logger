da526dec8162ffa1446fff9dac1bc083d091f928
B1tF8er <ofeth@hotmail.com>
2018-12-28 08:17:05 -0700

    Add DefaultFormatProvider unit tests

---
+++ Bit.Logger.Tests/DefaultFormatProviderShould.cs
@@ -0,0 +1,35 @@
+namespace Bit.Logger.Tests
+{
+    using Enums;
+    using FormatProviders;
+    using Moq;
+    using System;
+    using Xunit;
+
+    public class DefaultFormatProviderShould
+    {
+        private readonly Mock<DefaultFormatProvider> sut;
+
+        public DefaultFormatProviderShould()
+        {
+            sut = new Mock<DefaultFormatProvider>();
+        }
+
+        [Fact]
+        public void GetFormat_ObjectAsDefaultCustomFormatter_WhenTypeIsCustomFormatter()
+        {
+            var actual = sut.Object.GetFormat(typeof(ICustomFormatter));
+
+            Assert.NotNull(actual);
+            Assert.IsType<DefaultCustomFormatter>(actual);
+        }
+
+        [Fact]
+        public void GetFormat_ObjectAsNull_WhenTypeIsNotCustomFormatter()
+        {
+            var actual = sut.Object.GetFormat(typeof(IFormatProvider));
+
+            Assert.Null(actual);
+        }
+    }
+}
