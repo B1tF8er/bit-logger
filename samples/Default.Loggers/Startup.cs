a6ba81ac1506ec1102dc48eb1000193528b614e0
B1tF8er <ofeth@hotmail.com>
2019-04-12 21:20:22 -0600

    Refactor Startup class

--- samples/Default.Loggers/Startup.cs
+++ samples/Default.Loggers/Startup.cs
@@ -10,30 +10,28 @@ namespace Default.Loggers
 
     internal static class Startup
     {
-        private static readonly ServiceCollection serviceCollection;
-
         internal static ServiceProvider ServiceProvider { get; }
 
-        static Startup()
+        static Startup() => ServiceProvider = new ServiceCollection()
+            .AddServices()
+            .BuildServiceProvider();
+
+        private static ServiceCollection AddServices(this ServiceCollection serviceCollection)
         {
-            serviceCollection = new ServiceCollection();
-            AddServices();
-            ServiceProvider = serviceCollection.BuildServiceProvider();
+            serviceCollection
+                .AddSingleton(Logger())
+                .AddSingleton(Configuration())
+                .AddTransient<App>();
+
+            return serviceCollection;
         }
 
-        private static void AddServices()
-        {
-            serviceCollection.AddSingleton(Logger);
-            serviceCollection.AddSingleton(Configuration);
-            serviceCollection.AddTransient<App>();
-        }
-
-        private static ILogger Logger => new Logger()
+        private static ILogger Logger() => new Logger()
             .AddConsoleSource(CreateConfiguration(DateType.DateTimeIso, ShowLevel.Yes, Level.Trace))
             .AddDatabaseSource(CreateConfiguration(DateType.DateIso, ShowLevel.No))
             .AddFileSource(CreateConfiguration(DateType.TimeIso, ShowLevel.Yes, Level.Critical));
 
-        private static IConfiguration Configuration => new ConfigurationBuilder()
+        private static IConfiguration Configuration() => new ConfigurationBuilder()
             .SetBasePath(AppContext.BaseDirectory)
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
