bf568ad2c3064ee0770cc41f1c6c39f702a6bf0f
Jorge Yañez <ofeth@hotmail.com>
2020-12-02 15:41:38 -0700

    Improve log configuration creation

--- samples/Default.Loggers/LogConfigurationExtensions.cs
+++ samples/Default.Loggers/LogConfigurationExtensions.cs
@@ -2,16 +2,39 @@ namespace Default.Loggers
 {
     using Bit.Logger.Config;
     using Bit.Logger.Enums;
-    using static Bit.Logger.Enums.Level;
+    using System;
+    using System.IO;
+    using System.Linq;
 
     internal static class LogConfigurationExtensions
     {
-        internal static Configuration CreateConfiguration(DateType dateType, ShowLevel showLevel, Level level = Information) =>
+        internal static Configuration CreateConsoleLogConfiguration() =>
             new Configuration
             {
-                DateTypeFormat = dateType,
-                ShowLevel = showLevel,
-                Level = level
+                DateTypeFormat = DateType.DateTimeIso,
+                ShowLevel = ShowLevel.Yes,
+                Level = Level.Trace
             };
+
+        internal static Configuration CreateDatabaseLogConfiguration() =>
+                new Configuration
+                {
+                    DateTypeFormat = DateType.DateIso,
+                    ShowLevel = ShowLevel.Yes,
+                    Level = Level.Information,
+                    DatabaseLogLocation = GetCustomLogLocation("Database")
+                };
+
+        internal static Configuration CreateFileLogConfiguration() =>
+            new Configuration
+            {
+                DateTypeFormat = DateType.TimeIso,
+                ShowLevel = ShowLevel.No,
+                Level = Level.Critical,
+                FileLogLocation = GetCustomLogLocation("File")
+            };
+
+        private static string GetCustomLogLocation(string logType) =>
+            Path.Combine(Environment.GetLogicalDrives().FirstOrDefault(), "Logs", logType);
     }
 }
--- samples/Default.Loggers/Startup.cs
+++ samples/Default.Loggers/Startup.cs
@@ -2,12 +2,9 @@ namespace Default.Loggers
 {
     using Bit.Logger;
     using Bit.Logger.Contract;
-    using Bit.Logger.Enums;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using System;
-    using System.IO;
-    using System.Linq;
     using static Constants.Path;
     using static LogConfigurationExtensions;
 
@@ -15,41 +12,27 @@ namespace Default.Loggers
     {
         internal static ServiceProvider ServiceProvider { get; }
 
-        static Startup() => ServiceProvider = new ServiceCollection()
-            .AddServices()
-            .BuildServiceProvider();
+        static Startup() =>
+            ServiceProvider = new ServiceCollection()
+                .AddServices()
+                .BuildServiceProvider();
 
-        private static ServiceCollection AddServices(this ServiceCollection serviceCollection)
-        {
+        private static IServiceCollection AddServices(this ServiceCollection serviceCollection) =>
             serviceCollection
                 .AddSingleton(Logger())
                 .AddSingleton(Configuration())
                 .AddTransient<App>();
 
-            return serviceCollection;
-        }
+        private static ILogger Logger() =>
+            new Logger()
+                .AddConsoleSource(CreateConsoleLogConfiguration())
+                .AddDatabaseSource(CreateDatabaseLogConfiguration())
+                .AddFileSource(CreateFileLogConfiguration());
 
-        private static ILogger Logger()
-        {
-            var consoleConfiguration = CreateConfiguration(DateType.DateTimeIso, ShowLevel.Yes, Level.Trace);
-            var databaseConfiguration = CreateConfiguration(DateType.DateIso, ShowLevel.Yes);
-            var fileConfiguration = CreateConfiguration(DateType.TimeIso, ShowLevel.No, Level.Critical);
-
-            databaseConfiguration.DatabaseLogLocation = GetCustomLogLocation("Database");
-            fileConfiguration.FileLogLocation = GetCustomLogLocation("File");
-
-            return new Logger()
-                .AddConsoleSource(consoleConfiguration)
-                .AddDatabaseSource(databaseConfiguration)
-                .AddFileSource(fileConfiguration);
-        }
-
-        private static string GetCustomLogLocation(string logType) =>
-            Path.Combine(Environment.GetLogicalDrives().FirstOrDefault(), "Logs", logType);
-
-        private static IConfiguration Configuration() => new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile(AppSettingsPath, optional: true, reloadOnChange: true)
-            .Build();
+        private static IConfiguration Configuration() =>
+            new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(AppSettingsPath, optional: true, reloadOnChange: true)
+                .Build();
     }
 }
